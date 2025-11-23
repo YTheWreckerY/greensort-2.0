21ac2a3818d0fc740fdfa1b4a752d16f038f9e04
YTheWreckerY <aravmohapatra301@gmail.com>
2025-11-23 13:52:16 +0530

    Update server.py

--- server.py
+++ server.py
@@ -109,4 +109,5 @@ if __name__ == "__main__":
     # CORS for local dev convenience
     from flask_cors import CORS
     CORS(app, resources={r"/api/*": {"origins": "*"}})
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
